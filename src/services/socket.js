9dc2c5bb7c5b64a4ff7705d9cb0e26f128b2c386
Karamveer Singh <161586290+Karmveersingh2003@users.noreply.github.com>
2026-04-10 03:22:20 +0530

    Update socket.js

--- src/services/socket.js
+++ src/services/socket.js
@@ -1,6 +1,6 @@
 import { io } from 'socket.io-client';
 
-const SOCKET_URL = process.env.REACT_APP_API_URL || 'http://localhost:5000';
+const SOCKET_URL = process.env.REACT_APP_API_URL || 'https://hotelwhatsapp.vercel.app/';
 
 let socket = null;
 
